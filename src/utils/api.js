6172b74ae73204afc1de19fcaf780986f6ea323e
Riuqlav <valquirsolza.vs@gmail.com>
2022-05-26 20:25:16 +0200

    useEffect to fetch

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 //fetch all data from the API "https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json"
 
 const Api = () => {
   const [pllNumber, setPllNumber] = useState([]);
 
-  fetch(
-    "https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json"
-  )
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-      pllNumber(json);
-    });
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setPllNumber(data);
+      });
+  }, []);
 
-  const sortByPopulation = [...setPllNumber].sort(
+  const sortByPopulation = [...pllNumber].sort(
     (a, b) => a.population - b.population
   );
-  console.log(sortByPopulation);
 };
 
 export default Api;
