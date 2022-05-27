94a32e7994977279cc1782579867292711116f71
Riuqlav <valquirsolza.vs@gmail.com>
2022-05-27 12:06:14 +0200

    filter() > sort() > slice()

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import "./App.css";
 import Cards from "./components/Cards/Cards";
 import CardsHeader from "./components/Cards/CardsHeader";
 import Api from "./utils/Api";
+//import { GlobalContext } from "./utils/Api";
 
 function App() {
   return (
     <>
-      {/* globalContext sandwich the app to pass down state of Api to all components */}
-      {/* <globalContext.Provider value={Api}> */}
+      {/* globalContext sandwich the app to pass down state of Api to all components  */}
+      {/* <GlobalContext.Provider value={Api}> */}
       <Api />
       <div className="App">
         <CardsHeader />
         <Cards />
       </div>
-      {/* </globalContext.Provider> */}
+      {/* </GlobalContext.Provider> */}
     </>
   );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,4 @@
-import { useState, useEffect, useContext } from "react";
-import React from "react";
-
-// const GlobalContext = React.createContext();
+import { useState, useEffect } from "react";
 
 const Api = () => {
   const [pllNumber, setPllNumber] = useState([]);
@@ -16,22 +13,20 @@ const Api = () => {
         setPllNumber(data);
       });
   }, []);
+  // I found that removing the ones with 0 ppl with filter(), then sort() before slice() was the best option
+  const countriesWithPopulation = pllNumber.filter((x) => x.population > 0);
 
-  //sorts all data by population
-  const sortByPopulation = [...pllNumber].sort(
-    (a, b) => a.population - b.population
-  );
+  const low10Countries = countriesWithPopulation
+    .sort((a, b) => a.population - b.population)
+    .slice(0, 10);
 
-  //slice the 10 highest population countries
-  const top10 = [...sortByPopulation].slice(-10);
+  const top10Countries = countriesWithPopulation
+    .sort((a, b) => b.population - a.population)
+    .slice(0, 10)
+    .reverse(); //reverse to get the top 10 in the same order as the low10Countries
 
-  //slice the 10 lowest population countries greater than 0 in population (I had to hard code it, I'll try to make it dynamic later on )
-
-  const low10 = [...sortByPopulation].slice(4, 14);
-
-  console.table(top10);
-  console.table(low10);
+  console.table(top10Countries);
+  console.table(low10Countries);
 };
 
-// return <GlobalContext value={Api}></GlobalContext>
 export default Api;
