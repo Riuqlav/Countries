2fe465a78b0794b5c453abb2933fd95ef24448f4
Riuqlav <valquirsolza.vs@gmail.com>
2022-05-28 18:54:37 +0200

    Buttons ready

--- src/components/Cards/CardsAll.js
+++ src/components/Cards/CardsAll.js
@@ -28,21 +28,14 @@ const CardsAll = (props) => {
                       is 17.5
                     </p>
                     <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
+                      <div className="container mt-2">
                         <button
                           type="button"
-                          className="btn btn-sm btn-outline-secondary"
+                          className="btn btn-sm btn-outline-secondary "
                         >
-                          View
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          Edit
+                          View Population
                         </button>
                       </div>
-                      <small className="text-muted">9 mins</small>
                     </div>
                   </div>
                 </div>
