733e3f9c89725f8aa94e60be8c8f7f292312e3b5
Riuqlav <valquirsolza.vs@gmail.com>
2022-05-30 19:19:30 +0200

    form

--- src/components/Cards/Header.js
+++ src/components/Cards/Header.js
@@ -1,69 +1,24 @@
-import React, { useState, useContext } from "react";
-import { GlobalContext } from "../../App";
+import { Formik, Field, Form } from "formik";
 import CardsAll from "./CardsAll";
 import CardsTop from "./CardsTop";
 import CardsLow from "./CardsLow";
+import { GlobalContext } from "../../App";
+import { useState, useContext } from "react";
+
+//set up the sandwich of global context in here for the cards with apiCountries instead of app.js
 
 const Header = () => {
-  const [active, setActive] = useState("");
   const apiCountries = useContext(GlobalContext);
+
+  const [active, setActive] = useState("");
+  const [countries, setcountries] = useState([]);
 
   return (
     <>
       <header>
-        <div className="collapse bg-dark " id="navbarHeader">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-8 col-md-7 py-4">
-                <h4 className="text-white">About</h4>
-                <p className="text-muted">
-                  kfjazlkfnalkzejfnlkazjenflkanzefklnazelkfnalzkejnfaklzejnflkazneflkazneflkajzneflkaznefklaznefklazneflkazjenf
-                </p>
-              </div>
-              <div className="col-sm-4 offset-md-1 py-4">
-                <h4 className="text-white">Contact</h4>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="#" class="text-white">
-                      Follow on Twitter
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-white">
-                      Like on Facebook
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-white">
-                      Email me
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className="navbar navbar-dark bg-dark shadow-sm">
           <div className="container d-flex justify-content-between">
             <a href="#" className="navbar-brand d-flex align-items-center">
-              {/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                aria-hidden="true"
-                class="mr-2"
-                viewBox="0 0 24 24"
-                focusable="false"
-              >
-                <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
-                <circle cx="12" cy="13" r="4" />
-              </svg> */}
-
               <img
                 width="40"
                 height="40"
@@ -97,18 +52,20 @@ const Header = () => {
             this format: <b>Country : Population</b> and a flag <b>URL</b>
           </p>
           <p>
-            <input
-              id="inputCountry"
-              type="text"
-              placeholder="ABkistan : 1000000"
-              className="my-2 m-1"
-            />
-            <input
-              id="inputFlag"
-              type="text"
-              placeholder="Paste a URL of a flag"
-              className="my-2 m-1"
-            />
+            <Formik
+              initialValues={{ country: "", population: "" }}
+              onSubmit={async (values) => {
+                await new Promise((resolve) => setTimeout(resolve, 500));
+                console.log(JSON.stringify(values, null, 2));
+              }}
+            >
+              <Form>
+                <Field name="country" type="text" />
+                <Field name="population" type="number" />
+                <button type="submit">Add</button>
+              </Form>
+            </Formik>
+
             <br />
             <button
               onClick={() => setActive("all")}
