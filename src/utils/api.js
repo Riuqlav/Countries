d11ceda1dab249aaddc5aec28b422f92d71aff5d
Riuqlav <valquirsolza.vs@gmail.com>
2022-06-01 07:49:43 +0200

    Clena up

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,5 @@
 import { useState, useEffect } from "react";
 
-// const Api = () => {
-//   const [pllNumber, setPllNumber] = useState([]);
-//   //If only the low10Countries and top10Countries need to be updated I don't need to make countriesPopulation updated.
-//   //const [updatedCountries, setUpdatedCountries] = useState([]);
-
-//   useEffect(() => {
-//     fetch(
-//       "https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json"
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setPllNumber(data);
-//       });
-//   }, []);
-//   const countriesPopulation = () => pllNumber.filter((x) => x.population > 0);
-
-//   return countriesPopulation();
-// };
-
 const Api = () => {
   const [apiCountries, setApiCountries] = useState([]);
   //useEffect to fetch data and make sure it only runs once with [] empty array
